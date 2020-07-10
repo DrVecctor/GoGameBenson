191f1918757d185bc11c0930ddfee736e09accc3
gonanda <fernanda-oliveira@web.de>
2020-07-10 13:11:57 +0200

    fixing bugs

--- dlgo/goboard_slow.py
+++ dlgo/goboard_slow.py
@@ -4,18 +4,20 @@ from dlgo.gotypes import Player
 
 class Move():
 	def __init__(self, point=None, is_pass=False, is_resign=False):
-	assert (point is not None) ^ is_pass ^ is_resign
-	self.point = point
-	self.is_play = (self.point is not None)
-	self.is_pass = is_pass
-	self.is_resign = is_resign
+		assert (point is not None) ^ is_pass ^ is_resign
+		self.point = point
+		self.is_play = (self.point is not None)
+		self.is_pass = is_pass
+		self.is_resign = is_resign
 
 	@classmethod
-	del play(cls, point):
+	def play(cls, point):
 		return Move(point=point)
+
 	@classmethod
 	def pass_turn(cls):
 		return Move(is_pass=True)
+
 	@classmethod
 	def resign(cls):
 		return Move(is_resign=True)
@@ -24,9 +26,9 @@ class Move():
 
 class GoString():
 	def __init__(self, color, stones, liberties):
-	self.color = color
-	self.stones = set(stones)
-	self.liberties = set(liberties)
+		self.color = color
+		self.stones = set(stones)
+		self.liberties = set(liberties)
 
 	def remove_liberty(self, point):
 		self.liberties.remove(point)
@@ -41,15 +43,13 @@ class GoString():
 			self.color,
 			combined_stones,
 			(self.liberties | go_string.liberties) - combined_stones)
-	@propertiy
+
+	@property
 	def num_liberties(self):
 		return len(self.liberties)
 
-	def__eq__(self, other):
-		return isinstance(other, GoString) and \
-			self.color == other.color and \
-			self.stones == other.stones and \
-			self.liberties == other.liberties
+	def __eq__(self, other):
+		return isinstance(other, GoString) and self.color == other.color and self.stones == other.stones and self.liberties == other.liberties
 
 # Listing 3.5 Creating a Go Board instance 
 
@@ -61,31 +61,50 @@ class Board():
 
 # Listing 3.6 Checking neighboring points for liberties
 
-def place_stone()
-	assert self.is_on_grid(point)
-	assert self._grid.get(point) is None
-	adjacent_same_color = []
-	adjacent_opposite_color = []
-	liberties = []
-	for neighbor in point.neighbors():
-		if not self.is_on_grid(neighbors):
-			continue
-		neighbor_string = self._grid.get(neighbor)
-		if neighbor_string is None:
-			liberties.append(neighbor)
-		elif neighbor_string.color == player:
-			if neighbor_string not in adjacent_same_color:
-				adjacent_same_color.append(neighbor_string)
+	def place_stone():
+		assert self.is_on_grid(point)
+		assert self._grid.get(point) is None
+		adjacent_same_color = []
+		adjacent_opposite_color = []
+		liberties = []
+		for neighbor in point.neighbors():
+			if not self.is_on_grid(neighbors):
+				continue
+			neighbor_string = self._grid.get(neighbor)
+			if neighbor_string is None:
+				liberties.append(neighbor)
+			elif neighbor_string.color == player:
+				if neighbor_string not in adjacent_same_color:
+					adjacent_same_color.append(neighbor_string)
 		else:
 			if neighbor_string not in adjacent_opposite_color:
 				adjacent_opposite_color.append(neighbor_string)
-	new_string = Gostring(player, [point], liberties)
+		new_string = Gostring(player, [point], liberties)
+
+		for same_color_string in adjacent_same_color:
+			new_string = new_string.merged_with(same_color_string)
+		for new_string_point in new_string.stones:
+			self._grid[new_string_point] = new_string
+		for other_color_string in adjacent_opposite_color:
+			other_color_string.remove_liberty(point)
+		for other_color_string in adjacent_opposite_color:
+			if other_color_string.num_liberties == 0:
+				self._remove_string(other_color_string)
+
+	def _remove_string(self, string):
+		for point in string.stones:
+			for neighbor in point.neighbors():
+				neighbor_string = self._grid.get(neighbor)
+				if neighbor_string is None:
+					continue
+				if nieghbor_string is not string:
+					neighbor_string.add_liberty(point)
+			self._grid[point] = None
 
 # Listing 3.7 Utility methods for placing and removing stones
 
 		def is_on_grid(self, point):
-			return 1<= point.row <= self.num_rows and \
-				1<= point.col <= self.num_cols
+			return 1<= point.row <= self.num_rows and 1<= point.col <= self.num_cols
 
 		def get(self, point):
 			string = self._grid.get(point)
@@ -98,27 +117,3 @@ def place_stone()
 			if string is None:
 				return None
 			return string
-
-# Listing 3.8 Continuing our definition of place_stone
-
-for same_color_string in adjacent_same_color:
-	new_string = new_string.merged_with(same_color_string)
-for new_string_point in new_string.stones:
-	self._grid[new_string_point] = new_string
-for other_color_string in adjacent_opposite_color:
-	other_color_string.remove_liberty(point)
-for other_color_string in adjacent_opposite_color:
-	if other_color_string.num_liberties == 0:
-		self._remove_string(other_color_string)
-
-# Listing 3.9 Continuing our definition of place_stone 
-
-def _remove_string(self, string):
-	for point in string.stones:
-		for neighbor in point.neighbors():
-			neighbor_string = self._grid.get(neighbor)
-			if neighbor_string is None:
-				continue
-			if nieghbor_string is not string:
-				neighbor_string.add_liberty(point)
-		self._grid[point] = None
